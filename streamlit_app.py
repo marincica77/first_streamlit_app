11778c7646a6d82e91281bfe0315e0deb5b0d12d
marincica77 <136705839+marincica77@users.noreply.github.com>
2023-06-15 13:31:34 +0200

    Update streamlit_app.py
    new menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
-st.title('My Parents New Healthy Diner')
+st.title('My Mom's New Healthy Diner')
 
-st.header('Breakfast menu')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+st.header('Breakfast favorites')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
