f64491a2b477b199d757f35d8ed26e564c44f9bc
marincica77 <136705839+marincica77@users.noreply.github.com>
2023-06-16 11:54:16 +0200

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,13 +38,13 @@ except URLError as e:
 
 #st.stop()
 
-st.header("View our fruit list - Add your favorites:")
+st.header("View our fruit list - Add your favorites!")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
        my_cur.execute("select * from fruit_load_list")
        return my_cur.fetchall()
 
-if st.button('Get fruit list'):
+if st.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   my_cnx.close()
