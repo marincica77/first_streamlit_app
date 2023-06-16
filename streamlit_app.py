f790942c533b356feb0eed6ca9b604996fb3b417
marincica77 <136705839+marincica77@users.noreply.github.com>
2023-06-16 11:14:35 +0200

    Update streamlit_app.py
    add fruit function

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,18 +20,19 @@ fruits_selected=st.multiselect("Pick some fruits:",list(my_fruit_list.index),['A
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = rq.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+  fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+  
 st.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = st.text_input('What fruit would you like information about?')
   if not fruit_choice:
     st.error("Please select a fruit to get information.")
   else:
-    #st.write('The user entered ', fruit_choice)
-    fruityvice_response = rq.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-    # normalizes json data 
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-    # shows as table
-    st.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    st.dataframe(this_fruit_choice)
 except URLError as e:
   st.error()
 
