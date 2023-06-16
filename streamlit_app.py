f4e1e2696ce23edf6094ef86326feebaf6bb1e62
marincica77 <136705839+marincica77@users.noreply.github.com>
2023-06-16 10:17:30 +0200

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,6 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 st.header("The fruit load list contains:")
-st.dataframe(my_data_row)
+st.dataframe(my_data_rows)
 
 
